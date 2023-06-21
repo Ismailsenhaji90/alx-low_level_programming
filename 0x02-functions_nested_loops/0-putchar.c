c53cb0433d18e86f7fa15e5e3f867789c8bd238c
Ismailsenhaji90 <isenhaji@gmail.com>
2023-06-21 16:20:49 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *main - Entry point
+ *Discription: Print base 9
+ *Return: Always 0
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
