060615b302422637ece04d52eeec30c98f18239a
Ismailsenhaji90 <isenhaji@gmail.com>
2023-06-22 15:00:12 -0700

    0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
 * main - Test function pos or neg
 * Return: 0
 */
+
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
-}
+i	int i;
+	i = 0;
 
+	positive_or_negative(i);
+
+	return (0);
+}
